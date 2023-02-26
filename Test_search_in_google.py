20490b3606bf50855bd701775ce8cdac6e43f5f3
GrigoryevaNYu <grigorievanayu@gmail.com>
2023-02-26 22:40:28 +0300

    Fixed: delete 5th row in tests, add libs versions in requirements.txt

--- Test_search_in_google.py
+++ Test_search_in_google.py
@@ -2,7 +2,6 @@ from selene import browser, have
 
 
 def test1_source_found(browser_configs):
-
     browser.element('[name="q"]').type('Selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
